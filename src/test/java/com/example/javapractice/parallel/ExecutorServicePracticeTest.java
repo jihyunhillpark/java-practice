2d743db909e5239f8a7f703e5654082ce7391f7a
jihyunhillpark <girl9505@naver.com>
2024-04-11 01:58:19 +0900

    (sequential & parallel) add comment & code refactoring.

--- src/test/java/com/example/javapractice/parallel/ExecutorServicePracticeTest.java
+++ src/test/java/com/example/javapractice/parallel/ExecutorServicePracticeTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -14,18 +13,15 @@ public class ExecutorServicePracticeTest {
 
     @Test
     void sequential_process_when_simple_sequential_iteration() throws InterruptedException {
-        List<List<Integer>> targetNumbers = StubNumbers.getNumbers();
-
         long start = System.nanoTime();
-        Integer totalResult = 0;
-        for(List<Integer> numbers : targetNumbers) {
-            for(Integer number : numbers) {
-                totalResult += number;
-                Thread.sleep(10);
-            }
+        int totalResult = 0;
+        for (int number = 0; number <= 1000; number++) {
+            totalResult += number;
+            Thread.sleep(10);
         }
 
-        long duration = (System.nanoTime() - start)/1000000;
+        // 결과 출력
+        long duration = (System.nanoTime() - start) / 1000000;
         System.out.printf("sequential iteration = total : %s, timespent: %s msecs%n", totalResult, duration);
     }
 
@@ -33,22 +29,23 @@ public class ExecutorServicePracticeTest {
     void parallel_process_when_ExecutorService_is_fixedSizePool() throws InterruptedException, ExecutionException {
         List<List<Integer>> targetNumbers = StubNumbers.getNumbers();
         ExecutorService executor = Executors.newFixedThreadPool(10);
-        List<Callable<Integer>> tasks = new ArrayList<>();
 
         long start = System.nanoTime();
         int totalResult = 0;
+        // parallel : multi-threading
+        List<Future<Integer>> results = new ArrayList<>();
         for (List<Integer> numbers : targetNumbers) {
-            tasks.add(new SimpleNumberChunkCalculation(numbers));
+            results.add(executor.submit(new SimpleNumberChunkCalculation(numbers)));
         }
-        List<Future<Integer>> results = executor.invokeAll(tasks);
-        executor.shutdownNow();
 
         for (Future<Integer> result : results) {
-            totalResult += result.get();
+            totalResult += result.get(); // 비동기 작업의 결과를 가져오는 부분
             Thread.sleep(10);
         }
+        executor.shutdownNow(); // 이미 제출된 작업들을 인터럽트&종료
 
-        long duration = (System.nanoTime() - start)/1000000;
+        // 결과 출력
+        long duration = (System.nanoTime() - start) / 1000000;
         System.out.printf("parallel1 = total : %s, timespent: %s msecs%n", totalResult, duration);
     }
 }
